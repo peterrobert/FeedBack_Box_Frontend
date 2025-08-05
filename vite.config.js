46a273498cb1fabd8b29d2948a25770887cf763b
peterrobert <pwambui93@gmail.com>
2025-08-05 09:19:32 +0300

    `Update vite.config.js: remove unused code and update base path for production`

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // eslint-disable-next-line no-undef
-  base: process.env.NODE_ENV === "production" ? "/FeedBack_Box_Frontend" : "/",
-  define: {
-    // eslint-disable-next-line no-undef
-    "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-  },
+  base: "/FeedBack_Box_Frontend/", // Update base path for production
 });
