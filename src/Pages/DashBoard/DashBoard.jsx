83f01083a7170379dac4db3bacb6cdf7b94e1583
peterrobert <pwambui93@gmail.com>
2025-08-05 22:04:24 +0300

    `Refactored icon in DashBoard component from SVG to Material UI's LightbulbRoundedIcon`

--- src/Pages/DashBoard/DashBoard.jsx
+++ src/Pages/DashBoard/DashBoard.jsx
@@ -283,26 +283,9 @@ const DashBoard = () => {
                 <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-8">
                   <div className="bg-gray-50 rounded-lg p-4 text-center">
                     <div className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-status-new bg-opacity-10 mb-2">
-                      <i class="text-status-new" data-fa-i2svg="">
-                        <svg
-                          class="svg-inline--fa fa-lightbulb"
-                          aria-hidden="true"
-                          focusable="false"
-                          data-prefix="fas"
-                          data-icon="lightbulb"
-                          role="img"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 384 512"
-                          data-fa-i2svg=""
-                        >
-                          <path
-                            fill="currentColor"
-                            d="M272 384c9.6-31.9 29.5-59.1 49.2-86.2l0 0c5.2-7.1 10.4-14.2 15.4-21.4c19.8-28.5 31.4-63 31.4-100.3C368 78.8 289.2 0 192 0S16 78.8 16 176c0 37.3 11.6 71.9 31.4 100.3c5 7.2 10.2 14.3 15.4 21.4l0 0c19.8 27.1 39.7 54.4 49.2 86.2H272zM192 512c44.2 0 80-35.8 80-80V416H112v16c0 44.2 35.8 80 80 80zM112 176c0 8.8-7.2 16-16 16s-16-7.2-16-16c0-61.9 50.1-112 112-112c8.8 0 16 7.2 16 16s-7.2 16-16 16c-44.2 0-80 35.8-80 80z"
-                          ></path>
-                        </svg>
-                      </i>
+                      <LightbulbRoundedIcon color="primary" />
                     </div>
-                    <h3 class="text-2xl font-bold text-status-new">42</h3>
+                    <h3 className="text-2xl font-bold text-status-new">42</h3>
                     <p class="text-gray-600 text-sm">New</p>
                   </div>
 
